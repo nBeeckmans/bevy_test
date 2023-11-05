af0f3b9f7bcdea0550e3038d18da1d637fca95b9
Nicolas Beeckmans <70780656+nBeeckmans@users.noreply.github.com>
2023-11-04 22:31:56 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
-use bevy::{prelude::*, ecs::query, transform};
+use bevy::prelude::*; 
 
-fn main() -> () {
-    
+#[derive(Component)]
+struct Person; 
 
-    return (); 
+#[derive(Component)]
+struct Name(String); 
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("P1".to_string())));
+    commands.spawn((Person, Name("P2".to_string())));
+    commands.spawn((Person, Name("P3".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}", name.0);
+    }
+}
+
+fn hello_world() {
+    println!("test"); 
+}
+
+fn main(){
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, greet_people))
+        .run();
 }
